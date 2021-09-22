3f457e364cc45f844219594f211dcfa8784740ce
kaarumugam <70319196+kaarumugam@users.noreply.github.com>
2021-09-22 22:30:47 +0530

    Update angle_finer.py

--- angle_finer.py
+++ angle_finer.py
@@ -45,7 +45,7 @@ def lambda_handler(event, context):
 
 if __name__ == "__main__":
     time_val = sys.argv[1]
-    print("hi welcome")
+    print("hi welcome to chennai")
     event = {
     "time": time_val
     }
